de074c8bc1eea451a2e7ee80a049dd472bb9bfa8
Rohan Nelson <rohan.nelson@gmail.com>
2024-12-28 20:55:47 +0800

    Add Svelte to browser-extension

--- packages/browser-extension/vite.config.ts
+++ packages/browser-extension/vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [svelte()],
   build: {
     outDir: "dist",
     assetsDir: ".",
@@ -16,5 +17,6 @@ export default defineConfig({
         entryFileNames: `[name].js`,
       },
     },
+    target: "esnext",
   },
 });
