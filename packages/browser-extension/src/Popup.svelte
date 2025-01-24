9973fabf428f54a4d151d1d7206977b7c2d0e36c
Rohan Nelson <rohan.nelson@gmail.com>
2025-01-24 14:57:31 +0800

    Installed Tailwind typopgraphy plugin. Of two minds as to whether or not I'll actually use it...

--- packages/browser-extension/src/Popup.svelte
+++ packages/browser-extension/src/Popup.svelte
@@ -30,7 +30,7 @@ browser.runtime.onMessage.addListener((message: unknown): undefined => {
 </script>
 
 <main class="p-6">
-    <h1 class="text-2xl font-bold">Mealaroo</h1>
+    <h1 class=" prose-2xl">Mealaroo</h1>
     <button id="scrapeButton" type="button" onclick={handleClick} class="border border-amber-600 rounded-md p-1 text-sm font-semibold hover:bg-amber-600 hover:text-white z-auto pointer-events-auto">Scrape Recipe</button>
     {#if (ingredients || method)}<h2 class="text-xl font-bold">Scraping Results</h2>{/if}
     <Topic data={ingredients} heading={"Ingredients"}/>
