b5cdf61f52305186bce2149aeedef7647bb3d209
Rohan Nelson <rohan.nelson@gmail.com>
2025-06-10 21:05:58 +0800

    Defined initial schema! Will need to update API calls to match new schema (for another evening)

--- packages/browser-extension/src/test.ts
+++ packages/browser-extension/src/test.ts
@@ -3,8 +3,8 @@ import { trpc } from "api";
 const user = await trpc.userById.query(4);
 console.log("user:", user);
 const createdUser = await trpc.userCreate.mutate({
-  name: "Bobbo",
-  age: 63,
-  email: "Bobbo@bobbo.com",
+  name: "Bobby",
+  age: 64,
+  email: "Bobby@bobby.com",
 });
 console.log("Created User: ", createdUser);
--- packages/node-backend/db/schema.ts
+++ packages/node-backend/db/schema.ts
@@ -1,12 +1,70 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import {
+  boolean,
+  integer,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+export const families = pgTable("families", {
+  id: uuid().primaryKey().notNull(),
+});
+
+export const accounts = pgTable("accounts", {
+  id: uuid().primaryKey().notNull(),
   name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  familyId: uuid()
+    .notNull()
+    .references(() => families.id, { onDelete: "cascade" }),
+});
+
+export const members = pgTable("members", {
+  id: serial().primaryKey().notNull(),
+  name: text(),
+  familyId: uuid()
+    .notNull()
+    .references(() => families.id, { onDelete: "cascade" }),
+});
+
+export const recipes = pgTable("recipes", {
+  id: uuid().primaryKey().notNull(),
+  familyId: uuid()
+    .notNull()
+    .references(() => families.id, { onDelete: "cascade" }),
+  recipeName: text().notNull(),
+  description: text(),
+  sourceLabel: text(),
+  sourceUrl: text(),
+  totalTime: integer(),
+  prepTime: integer(),
+  cookTime: integer(),
+  serves: text(),
+  imageUrl: text(), //Should probably abstract image out into its own table...
+  imageAlt: text(),
+  method: text(),
+  notes: text(),
+  createdAt: timestamp().notNull().defaultNow(),
+  updatedAt: timestamp().notNull().defaultNow(),
+});
+
+export const ingredients = pgTable("ingredients", {
+  id: serial().primaryKey(),
+  recipeId: uuid().references(() => recipes.id, { onDelete: "cascade" }),
+  line: integer().notNull(),
+  isHeading: boolean()
+    .notNull()
+    .$default(() => false),
+  rawIngredient: text().notNull(),
 });
 
 export const schema = {
-  users: usersTable,
+  accounts,
+  families,
+  recipes,
+  ingredients,
+  members,
 };
